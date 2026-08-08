c040e7b7b459da89fb506cf512e074ea4b4a6364
Sukkk-zcy <x2901695491@163.com>
2026-08-08 17:22:23 +0800

    deploy: 2026-08-08 17:22

---
+++ _astro/BlogPost.astro_astro_type_script_index_0_lang.BNCdvdFp.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./preload-helper.CVfkMyKi.js";i(()=>import("./wikilink-preview.CrXzI4E9.js"),[]).catch(o=>{console.error("Failed to load wikilink preview:",o)});
---
+++ _astro/GlobalTagGraph.astro_astro_type_script_index_0_lang.DttZoB9I.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/global-tag-graph.Dx-VF1-F.js","_astro/preload-helper.CVfkMyKi.js"])))=>i.map(i=>d[i]);
+import{_ as a}from"./preload-helper.CVfkMyKi.js";a(async()=>{const{setupGlobalTagGraph:t}=await import("./global-tag-graph.Dx-VF1-F.js");return{setupGlobalTagGraph:t}},__vite__mapDeps([0,1])).then(({setupGlobalTagGraph:t})=>t());
